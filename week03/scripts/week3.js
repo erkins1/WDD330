1849b2c77f0f252ef669ebd5e39652044e1d4137
Geoffrey <rog13007@gmail.com>
2020-05-05 15:22:24 -0500

    Week 3 notes and example

--- week03/scripts/week3.js
+++ week03/scripts/week3.js
@@ -1,5 +1,59 @@
+"use strict";
 /* Reading examples */
 
+/* Ex. 1 - Balloon *
+document.body.addEventListener('keyup', (event) => {
+    console.log(event.key);
+    if(event.key == 'ArrowUp'){
+        growBalloon();
+    }
+    else if (event.key == 'ArrowDown'){
+        shrinkBalloon();
+    }
+});*/
+
+//Add the event listener
+document.body.addEventListener('keyup', balloonController);
+
+// Set the balloon to an initial size 
+document.getElementById('balloon').style.fontSize = 20 + "px";
+
+//Determine if the balloon grows or shrinks
+function balloonController(event){
+    console.log(event.key);
+    if(event.key == 'ArrowUp'){
+        growBalloon();
+    }
+    else if (event.key == 'ArrowDown'){
+        shrinkBalloon();
+    }
+}
+
+//Increase the font size of the balloon
+function growBalloon(){
+    let balloon = document.getElementById('balloon');
+    let size = parseInt(balloon.style.fontSize.replace("px", ""));
+
+    console.log(size);
+
+    if (size >= 55){
+        balloon.innerHTML = "💥";
+        document.body.removeEventListener('keyup', balloonController);
+    } else {
+        balloon.style.fontSize = (size * 1.1) + "px";
+    }
+
+
+}
+
+//Decrease the font size of the balloon
+function shrinkBalloon(){
+    let balloon = document.getElementById('balloon');
+    let size = parseInt(balloon.style.fontSize.replace("px", ""));
+
+    console.log(size);
+    balloon.style.fontSize = (size * .9) + "px";
+}
 
 /* TeamActivity */
 
